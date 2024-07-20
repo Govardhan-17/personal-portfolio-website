a9663a8b6c50cc2a78c982ab25d4cc40aad1bfc4
Govardhan-17 <govardhansunkari20@gmail.com>
2024-07-20 09:27:46 +0530

    Add files via upload

---
+++ TASK 4/javascript file t4.js
@@ -0,0 +1,11 @@
+// Optional: Add JavaScript for smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
